6ea91eb925cf56c4583d95564693b399dae38d6d
11335 <jack.lanning@vtsd.com>
2023-09-19 10:06:45 -0400

    Test File

---
+++ 1-1-2/casting practice.py
@@ -0,0 +1,16 @@
+guy = 1.999999
+now = 65
+um = "uh"
+
+print("Inital Values")
+print("guy:", guy)
+print("now:", now)
+print("um:", um)
+
+guy = int(guy)
+now = chr(now)
+
+print("Ending Values")
+print("guy:", guy)
+print("now:", now)
+print("um:", um)
